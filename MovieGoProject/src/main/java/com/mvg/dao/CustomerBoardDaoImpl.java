4a108a8183f3c17bfcb921a3010765f11d7c3b00
ce401-13 <ce401-13@ce401-13-HP>
2015-02-10 16:46:43 +0900

    CustomerBoardDaoImpl Test 진행중

--- MovieGoProject/src/main/java/com/mvg/dao/CustomerBoardDaoImpl.java
+++ MovieGoProject/src/main/java/com/mvg/dao/CustomerBoardDaoImpl.java
@@ -2,10 +2,25 @@ package com.mvg.dao;
 
 import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.mvg.entity.CustomerBoard;
 
+@Repository
 public class CustomerBoardDaoImpl implements CustomerBoardDao {
 
+	private static final Logger logger = LoggerFactory
+			.getLogger(CustomerBoardDaoImpl.class);
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	String nameSpace="com.mvg.mappers.customerBoardMapper.";
+	
 	@Override
 	public int getAllBoardCount() {
 		// TODO Auto-generated method stub
@@ -14,8 +29,10 @@ public class CustomerBoardDaoImpl implements CustomerBoardDao {
 
 	@Override
 	public List<CustomerBoard> getAllBoard() {
-		// TODO Auto-generated method stub
-		return null;
+		String stmt = nameSpace+"selectAllBoard";
+		List<CustomerBoard> results = sqlSession.selectList(stmt);
+		logger.trace(""+results);
+		return results;
 	}
 
 	@Override
@@ -37,7 +54,7 @@ public class CustomerBoardDaoImpl implements CustomerBoardDao {
 	}
 
 	@Override
-	public int delteCustomerBoard(int boardId) {
+	public int deleteCustomerBoard(int boardId) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
