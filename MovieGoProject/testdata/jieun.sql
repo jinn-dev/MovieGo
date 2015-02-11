8e4692e6c9aad32cf8ff9be39f63e47bf603cd39
ce401-13 <ce401-13@ce401-13-HP>
2015-02-11 14:29:47 +0900

    CommentMapper, CommentController 추가

---
+++ MovieGoProject/testdata/jieun.sql
@@ -0,0 +1,13 @@
+-- 회원
+INSERT INTO users (user_id, user_pwd, user_email, user_birthday, user_coupon, user_point) VALUES ('a', '1', 'a', '', 'n', '');
+
+
+-- 게시판
+INSERT INTO customerboards (user_id, board_title, board_content) VALUES ('a', '테스트중', '내용 불러오기 성공');
+
+
+-- 댓글
+INSERT INTO comments (board_id, user_id, comment_content) VALUES ('2', 'a', 'hihi');
+INSERT INTO comments (board_id, user_id, comment_content) VALUES ('2', 'a', 'hello');
+
+
