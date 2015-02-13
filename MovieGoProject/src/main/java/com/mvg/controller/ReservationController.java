4efc4043d54931c4edb29bbc16dd17f520d0c823
ce401-16 <ce401-16@ce401-16-HP>
2015-02-13 14:55:07 +0900

    ;

--- MovieGoProject/src/main/java/com/mvg/controller/ReservationController.java
+++ MovieGoProject/src/main/java/com/mvg/controller/ReservationController.java
@@ -1,16 +1,18 @@
 package com.mvg.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
-
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
+import com.mvg.entity.Theater;
 import com.mvg.service.ReservationService;
+import com.mvg.service.TheaterService;
 
 @Controller
 public class ReservationController {
@@ -21,10 +23,20 @@ public class ReservationController {
 	}
 
 	@Autowired
-	ReservationService service;
+	ReservationService rservice;
 	
-	@RequestMapping(value="/reservation",  method=RequestMethod.GET)
-	public String reservationTest(){
+	@Autowired
+	TheaterService tservice;
+	
+	
+	
+	@RequestMapping(value="/reserve",  method=RequestMethod.GET)
+	public String reserve(Model model){
+		List<Theater> theaters = tservice.getAllTheatersService();
+		model.addAttribute("theaters", theaters);
+		
 		return "reservation/reservation1";
 	}
+	
+	
 }
